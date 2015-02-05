86fd21b85cdd15e7f42bf9205c3f95d5a431509f
SamHiggy <SHignett1@uclan.ac.uk>
2015-02-05 15:24:15 +0000

    Adding Particle Emitter and Particle Class

---
+++ DefiantWorlds/ProfSkillsDefiantWorlds/ParticleEmitter.h
@@ -0,0 +1,40 @@
+//-----------------------------------------------------
+// FILE: ParticleEmitter.h
+//-----------------------------------------------------
+
+#ifndef PARTICLE_EMITTER_H
+#define PARTICLE_EMITTER_H
+
+//-----------------------------------------------------
+// INCLUDES
+//-----------------------------------------------------
+#include "Common.h"
+
+//-----------------------------------------------------
+// CONSTANTS
+//-----------------------------------------------------
+const int MAX_PARTICLE_NUMBER = 1000;
+
+//-----------------------------------------------------
+// PARTICLE EMITTER CLASS
+//-----------------------------------------------------
+class CParticleEmitter
+{
+protected:
+	// EMITTER DATA
+	//---------------------------
+	int mParticleNumber;
+	float mParticleSpeed;
+	vector<float> test;
+
+	//EMITTER MODEL SOURCE
+	IModel* parent;
+	
+	// VIRTUAL METHODS
+	//---------------------------
+	void UpdateSystem();
+
+};
+
+
+#endif
