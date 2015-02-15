914b34dbe3fb83ceb122e9c94b18d0675c6e932d
mhinchcliffe <michael.hinchcliffe@hotmail.co.uk>
2015-02-15 15:50:15 +0000

    Filled out some of the basic functions of fleet, so it can handle basic fights.

--- DefiantWorlds/ProfSkillsDefiantWorlds/Fleet.cpp
+++ DefiantWorlds/ProfSkillsDefiantWorlds/Fleet.cpp
@@ -9,3 +9,46 @@ CFleet::CFleet()
 CFleet::~CFleet()
 {
 }
+
+
+void CFleet::Fight()
+{
+	
+	for (int i = 0; i < mSize; i++)
+	{
+		CRandomiser shipToAttack;
+		int target = shipToAttack.GetRandomInt(0, mpEnemyFleet->GetSize() - 1);
+		mpFleet[i]->Attack(mpEnemyFleet->GetShip(target), mHitMod, mDamegMod);
+	}
+
+}
+
+void CFleet::UpdateCondition()
+{
+	for (int i = 0; i < mSize; i++)
+	{
+		if (mpFleet[i]->GetHealth() < 0.0f)
+		{
+			CSpaceUnit*temp = mpFleet[i];
+			mpFleet[i] = mpFleet[mSize - 1];
+			mpFleet.pop_back();
+			delete temp;
+			mSize--;
+		}
+	}
+}
+
+CSpaceUnit* CFleet::GetShip(int i)
+{
+	return mpFleet[i];
+}
+
+int CFleet::GetSize()
+{
+	return mSize;
+}
+
+void CFleet::SetEnemy(CFleet* myEnemy)
+{
+	mpEnemyFleet = myEnemy;
+}
