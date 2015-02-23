28a834632117bc7b899a2e45b834c07ee0c81a2c
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-02-23 10:53:27 +0000

    Continued Player System Re-write

---
+++ DefiantWorldsGame/Code/Player.cpp
@@ -0,0 +1,125 @@
+//-----------------------------------------------------
+// FILE: Player.cpp
+//-----------------------------------------------------
+
+
+//-----------------------------------------------------
+// INCLUDES
+//-----------------------------------------------------
+#include "Player.h"
+
+
+//-----------------------------------------------------
+// BASE PLAYER CLASS CONSTRUCTORS & DESTRUCTOR
+//-----------------------------------------------------
+CPlayer::CPlayer()
+{
+	mNumMinerals = 10000;
+	mpFleet = new CFleet();
+	CSpaceFighter* Temp;
+	for (int i = 0; i < 10; i++)
+	{
+		Temp = new CSpaceFighter();
+		mpSpaceUnitsList.push_back(Temp);
+	}
+}
+
+CPlayer::~CPlayer()
+{
+	for (mpiterStructures = mpStructureList.begin(); mpiterStructures != mpStructureList.end(); mpiterStructures++)
+	{
+		// Remove all models
+		(*mpiterStructures)->UnloadIModel();
+	}
+}
+
+
+//-----------------------------------------------------
+// PLAYER CLASS MUTATORS
+//-----------------------------------------------------
+bool CPlayer::MineralTransaction(int amount)
+{
+	if (mNumMinerals + amount < 0)
+	{
+		// Not enough funds for transaction
+		return false;
+	}
+	else
+	{
+		// Enough funds
+		mNumMinerals += amount;
+		return true;
+	}
+}
+
+
+//-----------------------------------------------------
+// PLAYER CLASS METHODS
+//-----------------------------------------------------
+bool CPlayer::PurchaseStructure(CStructure* pStructure, CGrid* pGrid, CTile* pTile)
+{
+	// Check whether new building can be afforded
+	if (mNumMinerals - pStructure->GetBuildCost() < 0)
+	{
+		// Not enough minerals - return relevant error (* TO DOOOOOOO *)
+		return false;
+	}
+
+	// Check if building area is free
+	if (!pStructure->TestStructureArea(pGrid, pTile))
+	{
+		// Something blocking construction - return relevant error (* TO DOOOOOOO *)
+		return false;
+	}
+
+	// Check to ensure no vehicles in the nearby area
+
+
+	// Everything fine - build & add to vector
+	pStructure->CreateStructure(pGrid);
+	pStructure->SetBuildLocation(pTile->GetGridPos());
+	mpStructureList.push_back(pStructure);
+	return true;
+}
+
+bool CPlayer::QueueUnit(CStructure* structure, CGameAgent* unit)
+{
+	return false;
+}
+
+void CPlayer::LaunchAttack()
+{
+	mpSpaceUnitsList = mpFleet->LaunchFleet(mpSpaceUnitsList);
+	//transition into space state from hear
+}
+
+CStructure* CPlayer::CheckStructureSelection(DX::XMFLOAT3 pos)
+{
+	// Loop through all structures
+	for (mpiterStructures = mpStructureList.begin(); mpiterStructures != mpStructureList.end(); mpiterStructures++)
+	{
+		// If there is a collision, return the pointer to that object
+		if ((*mpiterStructures)->PointCollision(pos))
+		{
+			return (*mpiterStructures);
+		}
+	}
+
+	return nullptr;
+}
+
+void CPlayer::Update()
+{
+	// Loop through all structures & update them
+	for (mpiterStructures = mpStructureList.begin(); mpiterStructures != mpStructureList.end(); mpiterStructures++)
+	{
+		if (!(*mpiterStructures)->Update())
+		{
+			// The current structure has been destroyed
+			CStructure* tmp = (*mpiterStructures);
+			SafeDelete(tmp);
+			mpStructureList.erase(mpiterStructures);
+			break;
+		}
+	}
+}
---
+++ DefiantWorldsGame/Code/Player.h
@@ -0,0 +1,124 @@
+//-----------------------------------------------------
+// FILE: Player.h
+//-----------------------------------------------------
+
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+
+//-----------------------------------------------------
+// INCLUDES
+//-----------------------------------------------------
+#include "SpaceFighter.h"		// Space unit
+#include "Mothership.h"			// Space unit
+#include "Transport.h"			// Space unit
+#include "Worker.h"				// Ground unit
+#include "Infantry.h"			// Ground unit
+#include "Tank.h"				// Ground unit	
+#include "Artillery.h"			// Ground unit
+#include "Bomber.h"				// Air unit
+#include "Fighter.h"			// Air unit
+
+#include "Fleet.h"				// Space Ship container
+
+#include "Minerals.h"			// Resource
+
+#include "House.h"				// Static structure
+#include "Barracks.h"			// Production structure
+#include "ComCentre.h"			// Production structure
+#include "Hellipad.h"			// Production structure
+#include "SpaceCentre.h"		// Production structure
+
+
+//-----------------------------------------------------
+// BASE CLASS FOR PLAYERS
+//-----------------------------------------------------
+class CPlayer
+{
+protected:
+	// DATA
+	//---------------------------
+	std::string mName;
+	int         mNumMinerals;
+	int         mCurPopLimit;
+	int         mCurPop;
+
+
+	// LISTS
+	//---------------------------
+	std::vector<CStructure*> mpStructureList;
+	std::vector<CGameAgent*> mpWorldUnitsList;
+	std::vector<CSpaceUnit*> mpSpaceUnitsList;
+
+	std::vector<CStructure*>::iterator mpiterStructures;
+	std::vector<CGameAgent*>::iterator mpiterGameAgents;
+	std::vector<CSpaceUnit*>::iterator mpiterSpaceUnits;
+
+	CFleet* mpFleet;
+public:
+	// CONSTRUCTORS & DESTRUCTOR
+	//---------------------------
+	CPlayer();
+	virtual ~CPlayer();
+
+
+	// ACCESSORS
+	//---------------------------
+	inline std::string GetPlayerName()
+	{
+		return mName;
+	}
+
+	inline int GetMineralAmount()
+	{
+		return mNumMinerals;
+	}
+
+	inline int GetCurrentPop()
+	{
+		return mCurPop;
+	}
+
+	inline int GetCurrentPopLimit()
+	{
+		return mCurPopLimit;
+	}
+
+	inline CFleet* GetFleet()
+	{
+		return mpFleet;
+	}
+
+	inline std::vector<CSpaceUnit*> GetSpaceUnitList()
+	{
+		return mpSpaceUnitsList;
+	}
+
+	// MUTATORS
+	//---------------------------
+	inline void SetPlayerName(std::string name)
+	{
+		mName = name;
+	}
+
+	inline void SetSpaceUnitList(std::vector<CSpaceUnit*> newUnits)
+	{
+		for (int i = 0; i < newUnits.size(); i++)
+		{
+			mpSpaceUnitsList.push_back(newUnits[i]);
+		}
+	}
+	bool MineralTransaction(int amount);
+
+
+	// METHODS
+	//---------------------------
+	bool PurchaseStructure(CStructure* pStructure, CGrid* pGrid, CTile* pTile);
+	CStructure* CheckStructureSelection(DX::XMFLOAT3 pos);
+	bool QueueUnit(CStructure* structure, CGameAgent* unit);
+	void LaunchAttack();
+	void Update();
+};
+
+
+#endif /* _PLAYER_H_ */
