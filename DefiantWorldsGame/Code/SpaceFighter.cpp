17c635e592f5cc84f29dc328b202b7275fc1f7c8
mhinchcliffe <michael.hinchcliffe@hotmail.co.uk>
2015-03-26 16:18:29 +0000

    fixed LAZERERS

--- DefiantWorldsGame/Code/SpaceFighter.cpp
+++ DefiantWorldsGame/Code/SpaceFighter.cpp
@@ -106,7 +106,7 @@ void CSpaceFighter::MoveY(float yChange)
 //-----------------------------------------------------
 bool CSpaceFighter::Attack(CGameObject* target, float hitMod, float damageMod)
 {
-	if (mChargingLazers) return false;
+	if (!mCharged) return false;
 	if (mpToHitRoll->GetRandomFloat(1.0, 100.0) < (hitMod*mHitChance) * 100)
 	{
 		target->TakeDamage(mDamage*damageMod);
@@ -116,6 +116,8 @@ bool CSpaceFighter::Attack(CGameObject* target, float hitMod, float damageMod)
 		FireLazer(target);
 		return true;
 	}
+	mChargingLazers = true;
+	mCharged = false;
 	return false;
 }
 
@@ -135,16 +137,6 @@ void CSpaceFighter::HitFlash()
 	{
 		mpTempShield = mspMshSheild->CreateModel(mWorldPos.x, mWorldPos.y, mWorldPos.z);
 		mpTempShield->Scale(mScale + 0.05f);
-
-		/*if (mWorldPos.x < 0.0f)
-		{
-			mpTempShield->RotateY(90.0f);
-		}
-		else
-		{
-			mpTempShield->RotateY(-90.0f);
-		}*/
-
 		mpTempShield->RotateX(-35.0f);
 	}
 }
