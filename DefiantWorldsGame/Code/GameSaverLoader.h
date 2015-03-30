1c7c3a91a9df21930a5f6b41dcc73c98de85e066
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-03-30 11:29:06 +0100

    Added game saving & loading class

---
+++ DefiantWorldsGame/Code/GameSaverLoader.h
@@ -0,0 +1,31 @@
+//-----------------------------------------------------
+// FILE: GameSaverLoader.h
+//-----------------------------------------------------
+
+#ifndef _GAME_SAVER_LOADER_H_
+#define _GAME_SAVER_LOADER_H_
+
+
+//-----------------------------------------------------
+// INCLUDES
+//-----------------------------------------------------
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+//-----------------------------------------------------
+// GAME SAVER & LOADER CLASS
+//-----------------------------------------------------
+class CGameSaverLoader
+{
+public:
+
+
+
+private:
+
+};
+
+
+#endif /* _GAME_SAVER_LOADER_H_ */
