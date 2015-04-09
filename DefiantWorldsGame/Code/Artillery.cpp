fe675ef1486391ae65ab1a44ad700ba2de5b1cb6
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-04-09 11:37:14 +0100

    Artillery using correct collision boxes for attacks

--- DefiantWorldsGame/Code/Artillery.cpp
+++ DefiantWorldsGame/Code/Artillery.cpp
@@ -27,10 +27,7 @@ CArtillery::CArtillery()
 	mDamage = 20.0f;
 	mFireRate = 0.333333f;
 	mAttackTimer = 1.0f / mFireRate;
-	//mAttackParticleFX;
-	//mDestroyParticleFX;
 	mState = OBJ_CONSTRUCTING;
-	//mDestGridSq;
 	mIsMoving = false;
 	mHasPathTarget = false;
 	mAttackTarget = nullptr;
@@ -83,13 +80,31 @@ bool CArtillery::Attack(CGameObject* target, float hitMod, float damageMod)
 {
 	if (mAttackTimer >= (1.0f / mFireRate))
 	{
-		mAttackTimer = 0.0f;
-		SProjectile* newProjectile = new SProjectile();
-		newProjectile->mModel = mspMshArtilleryShell->CreateModel(mWorldPos.x, mWorldPos.y, mWorldPos.z);
-		newProjectile->mModel->LookAt(mAttackTarget->GetModel());
-		newProjectile->mSpeed = 50.0f;
+		// Check to see if the worker is close enough to the target to be able to attack it
+		float distance = 100.0f;
 
-		mpProjectiles.push_back(newProjectile);
+		// Get the local Z axis of the worker unit
+		DX::XMFLOAT4X4 objMatrix;
+		mpObjModel->GetMatrix(&objMatrix.m[0][0]);
+		DX::XMFLOAT3 localZ{ objMatrix.m[2][0], objMatrix.m[2][1], objMatrix.m[2][2] };
+
+		// Normalise this local axis
+		DX::XMVECTOR vecNormal = DX::XMVector4Normalize(DX::XMLoadFloat3(&localZ));
+		DX::XMStoreFloat3(&localZ, vecNormal);
+
+		// If the target is being looked at and is within range
+		if (mAttackTarget->RayCollision(mWorldPos, localZ, distance) && distance <= mRange)
+		{
+			SProjectile* newProjectile = new SProjectile();
+			newProjectile->mModel = mspMshArtilleryShell->CreateModel(mWorldPos.x, mWorldPos.y, mWorldPos.z);
+			//newProjectile->mModel->LookAt(mAttackTarget->GetModel());
+			newProjectile->mDirection = localZ;
+			newProjectile->mSpeed = 50.0f;
+
+			mpProjectiles.push_back(newProjectile);
+		}
+
+		mAttackTimer = 0.0f;
 	}
 	else
 	{
