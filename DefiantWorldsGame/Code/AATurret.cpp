132247e6efde9d381d4d40bc55521d4c8194c764
SamHiggy <SHignett1@uclan.ac.uk>
2015-04-26 13:34:49 +0100

    Minor AA Turret smoke change

--- DefiantWorldsGame/Code/AATurret.cpp
+++ DefiantWorldsGame/Code/AATurret.cpp
@@ -49,7 +49,7 @@ bool CTurretStructure::Update(CRTSPlayer* pPlayer)
 		{
 			if (mWarningSmoke == nullptr)
 			{
-				mWarningSmoke = new CSmoke(mpObjModel, 30, 20.0f, 0.8f);
+				mWarningSmoke = new CSmoke(mWorldPos, mHeight, 0.8f, mBoundingBox.GetWidthExtent() / 3.0f, mBoundingBox.GetLengthExtent() / 3.0f);
 			}
 			else
 			{
